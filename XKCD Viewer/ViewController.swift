8288acbb23db6933391bd0c5faffa5760e904a1d
20074556 <20074556@IT103iMAC13.wit.ie>
2019-10-02 17:10:30 +0100

    starting the image part

--- XKCD Viewer/ViewController.swift
+++ XKCD Viewer/ViewController.swift
@@ -8,11 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+var pictures = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items where item.hasSuffix(".png") {
+            pictures.append(item)
+        }
         // Do any additional setup after loading the view, typically from a nib.
+        print(pictures)
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        .replacingOccurrences(of:".png", with: "")
+        .components(separatedBy:"_") [2...]
+        .joined(separator: " ")
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
